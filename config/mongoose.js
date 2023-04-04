155328f20a4079eff22754a4b2d83c2187b23591
Monomay <monomoy230@gmail.com>
2023-04-04 18:55:13 +0530

    first backe end project

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const DB = 'mongodb://127.0.0.1:27017/PMS';
+
+
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log('connection successful')
+}).catch((err)=>{
+   console.log(err)            
+})
